9b6fa6af56f51be940d5fe545b9c3f41d6586fef
jaserali67 <143017747+jaserali67@users.noreply.github.com>
2023-09-04 19:45:11 +0300

    jhl

---
+++ scirpt.js
@@ -0,0 +1,32 @@
+alert('مرحبا بك في موقعي!انا اكتب (اسمك). وسوف احسب درجتك')
+let grade =prompt ('ادخل درجتك')
+console.log(grade)
+if(grade >= 90) {
+
+
+alert ('لقد حصلت على امتياز')}
+
+
+ if(grade >=80&& grade <=89){
+alert('لقد حصلت على جيد جدا')
+}
+
+else if(grade>=70&& grade <=79){
+    alert('لقد حصلت على جيد')
+}
+
+else if(grade>=60&& grade<=69){
+    alert('لقد حصلت على مقبول')
+}
+
+else if (grade>=50&& grade<=59){
+    alert('لقد حصلت على ضعيف')
+}
+
+else{
+    alert('لقد حصلت على راسب')
+}
+
+
+
+// جافا سكربت ليست جافا /
